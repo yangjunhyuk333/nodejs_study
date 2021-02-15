6d209dbadff1e1c09db2d9a0559d6783c70cc6be
yangjunhyuk333 <chuchoun1234@gmail.com>
2021-02-15 23:01:04 +0900

    [UPDATE]thirdDay

--- secondDay/secondDay.js
+++ secondDay/secondDay.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
-var data = fs.readFileSync('input.txt');
+var data = fs.readFileSync('D:/project/Cafeterian/Server/nodejs_test/secondDay/input.txt');
 
 console.log(data.toString());
 console.log("Program has ended");
